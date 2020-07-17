89162ae4918751163023f1450f88c899fed6fc58
unknown <AzureTest@vlianch001.redmond.corp.microsoft.com>
2020-07-17 01:26:14 -0700

    'WebApplication56'

---
+++ WebApplication56/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApplication56.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
